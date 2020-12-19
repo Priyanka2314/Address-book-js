8a474205ad4949cdced7fdc77c0083d68156362a
Priyanka <priyankacs102@gmail.com>
2020-12-19 07:34:47 +0530

    UC3 creating instance and an array to add details

--- addressBook.js
+++ addressBook.js
@@ -84,7 +84,7 @@ class AddressBook{
         return this._zip;
       }
       set zip(zip){
-          let zipRegex = RegExp("^[1-9]{3}[[ ]*[0-9]{3}$");
+          let zipRegex = RegExp("^[1-9]{3}[ ]*[0-9]{3}$");
           if(zipRegex.test(zip))
            this._zip = zip;
           else 
@@ -108,17 +108,34 @@ class AddressBook{
         return this._email;
       }
       set email(email){
-          let emailRegex = RegExp("^([]a-z0-9A-Z)+([.]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-z0-9A-Z]{2,}([.]{1}[a-z]{2,})?$");
-          if(stateRegex.test(email))
+          let emailRegex = RegExp("^([a-z0-9A-Z])+([.]?[a-z0-9A-Z]+)*[@]{1}[a-z0-9A-Z]+[.]{1}[a-z0-9A-Z]{2,}([.]{1}[a-z]{2,})?$");
+          if(emailRegex.test(email))
            this._email = email;
           else 
             throw "Invalid email";
       }
     //defining to string method
     toString(){
-        return "First Name: "+this.firstName + " ,Last Name: "+this.lastName
-          + "\nAddress: "+this.address+" ,City: "+this.city+ " ,State: "+this.state+" ,Zip: "+this.zip
-          +"\nPhoneNumber: "+this.phoneNo+" ,Email: "+this.email;
+        return "\nFirst Name: "+this.firstName + " Last Name: "+this.lastName
+          + "\nAddress: "+this.address+" ,City: "+this.city+ " State: "+this.state+" Zip: "+this.zip
+          +"\nPhoneNumber: "+this.phoneNo+" Email: "+this.email;
     }
+}
+try{
+    //UC3 creating an array and add contact details
+    let  details = new AddressBook("Priyanka","Singh","Bina","Sonbhadra","UttarPradesh","231220",
+        "91 9450898791", "priyankacs102@gmail.com");
+        console.log(details.toString());
+    //UC3 adding details to array
+    let contactDetailsArray = new Array();
+    contactDetailsArray.push(new AddressBook("Praveen","Singh","Bina","Sonbhadra","UttarPradesh","231220", "91 9879504273","praveen@gmail.com"));
+    contactDetailsArray.push(new AddressBook("Prashant","Singh","Rewa","Rewa","MadheyPradesh","486001", "91 8879504273","prashant@gmail.com"));
+    contactDetailsArray.push(new AddressBook("Grijesh","Gautam","MaharajGanj","Gorakhpur","UttarPradesh","273001", "91 8589504273","grijesh@gmail.com"));
+    contactDetailsArray.push(new AddressBook("Ankur","Yadav","Banaras","Varanasi","UttarPradesh","231233", "91 7779504273","ankur@gmail.com"));
 
+    //printing array
+    contactDetailsArray.forEach((details) => console.log(details.toString()));
+
+}catch(e){
+    console.log(e);
 }
